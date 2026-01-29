a607e305914e774f148de2ff32ace2bbdc3ae785
parkminje <alswp6@naver.com>
2026-01-29 09:42:29 +0900

    수정: Spring Boot Repository 스캔 설정 오류 해결
    - TabletennisApplication 클래스를 루트 패키지로 이동
    - @EntityScan과 @EnableJpaRepositories 추가
    - Repository Bean 인식 문제 해결

--- src/main/java/com/yourcompany/pingpong/TabletennisApplication.java
+++ src/main/java/com/yourcompany/pingpong/TabletennisApplication.java
@@ -1,12 +1,14 @@
 // src/main/java/com/yourcompany/pingpong/TabletennisApplication.java
-package com.yourcompany.pingpong.common;
+package com.yourcompany.pingpong;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan; // ⭐⭐ import 추가 ⭐⭐
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.yourcompany.pingpong") // ⭐⭐ 이 부분이 핵심! 전체 패키지를 강제로 스캔 ⭐⭐
+@EntityScan(basePackages = "com.yourcompany.pingpong.domain")
+@EnableJpaRepositories(basePackages = "com.yourcompany.pingpong.modules")  // ⭐ modules 전체 스캔!
 public class TabletennisApplication {
     public static void main(String[] args) {
         SpringApplication.run(TabletennisApplication.class, args);
